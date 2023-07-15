8602b4ea003a7328d56ddc182407e778f12a2106
Ümit Yetişir <umitytsr@gmail.com>
2023-07-15 13:19:53 +0300

    second commit

---
+++ app/src/main/java/com/umitytsr/dictionaryapp/ui/detailer/DetailerAdapter.kt
@@ -0,0 +1,70 @@
+package com.umitytsr.dictionaryapp.ui.detailer
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.umitytsr.dictionaryapp.R
+import com.umitytsr.dictionaryapp.data.model.remote.meaning.WordMeaningResponse
+import com.umitytsr.dictionaryapp.databinding.ItemRowMeaningBinding
+import com.umitytsr.dictionaryapp.databinding.ItemRowWordBinding
+import com.umitytsr.dictionaryapp.util.Constants
+
+class DetailerAdapter(wordMeaning: WordMeaningResponse): RecyclerView.Adapter<DetailerAdapter.WordViewHolder>() {
+
+    private val word = wordMeaning.word
+    private val partOfSpeech = wordMeaning.meanings.partOfSpeech
+    private val definition = wordMeaning.meanings.definitions.definition
+
+
+    inner class WordViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bind(
+            word: String,
+            partOfSpeech: String,
+            definition: String
+            ){
+            if (adapterPosition == Constants.ITEM_WORD){
+                val binding = ItemRowWordBinding.bind(itemView)
+                binding.wordText.text = word
+            }else if (adapterPosition == Constants.ITEM_MEANING){
+                val binding = ItemRowMeaningBinding.bind(itemView)
+                with(binding) {
+                    partOfSpeechText.text = partOfSpeech
+                    wordMeaning.text = definition
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view : View = when(viewType){
+            Constants.ITEM_WORD -> layoutInflater.inflate(R.layout.item_row_word,parent,false)
+            Constants.ITEM_MEANING -> layoutInflater.inflate(R.layout.item_row_meaning,parent,false)
+            else -> throw IllegalArgumentException()
+        }
+        return WordViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        holder.bind(
+            word = word[position],
+            partOfSpeech = partOfSpeech[position],
+            definition = definition[position]
+        )
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) {
+            Constants.ITEM_WORD
+        }else if (position == 1) {
+            Constants.ITEM_MEANING
+        }else{
+            Constants.ITEM_SYNONYMS
+        }
+    }
+}
--- app/src/main/java/com/umitytsr/dictionaryapp/util/Constants.kt
+++ app/src/main/java/com/umitytsr/dictionaryapp/util/Constants.kt
@@ -3,4 +3,7 @@ package com.umitytsr.dictionaryapp.util
 object Constants {
     const val BASE_URL_MEANING = "https://api.dictionaryapi.dev/api/"
     const val BASE_URL_SYNONYMS = "https://api.datamuse.com/"
+    const val ITEM_WORD : Int = 0
+    const val ITEM_MEANING : Int = 1
+    const val ITEM_SYNONYMS : Int = 2
 }
